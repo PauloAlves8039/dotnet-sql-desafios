ea181ac10768fdda5f14c837ab85dbbe4edb52d3
PualoAlves8039 <pj38alves@gmail.com>
2023-08-02 16:04:58 -0300

    Aplicando solução para o segundo desafio

---
+++ src/Desafios.Tarefa2/Tarefa2.sql
@@ -0,0 +1,25 @@
+-- A primeira query vai retornar a contagem de registros para a tabela "Pedido" usando o alias "Total".
+SELECT COUNT(Codigo) as Total
+FROM Pedido;
+
+-- O seu resultado é: Total = 100
+
+-- A segunda query vai retornar a contagem de registros referentes ao campo "CodigoComprador" para a tabela "Pedido" usando o alias "Total123".
+SELECT COUNT(Codigo) as Total123
+FROM Pedido 
+WHERE CodigoComprador = 123;
+
+-- O seu resultado é: Total123 = 15
+
+-- A terceira query vai trazer a contagem de registos referentes ao campo "CodigoComprador" na tabela "Pedido" usando o alias "TotalFinal" para os valores diferentes de 123.
+SELECT COUNT(Codigo) as TotalFinal 
+FROM Pedido 
+WHERE CodigoComprador <> 123
+
+
+/*
+   Para encontrar o resultado da terceira query precisamos subtrair o valor retornado na segunda query (Total123) com o valor retornado da primeira query (Total).
+   TotalFinal = Total - Total123 
+   Com isso encontraremos o número de registros em que o campo "CodigoComprador" seja diferente de 123.
+   Temos o resultado: TotalFinal = 85
+*/
