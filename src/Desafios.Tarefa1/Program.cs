eff7157738adb7f844bfec5e3c2240ecb2e8d373
PualoAlves8039 <pj38alves@gmail.com>
2023-08-02 15:57:40 -0300

    Aplicando solução para o primeiro desafio

---
+++ src/Desafios.Tarefa1/Program.cs
@@ -0,0 +1,73 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arrayNumber = { 7, 5, 3, 9, 6, 4, 1 };
+
+        Console.Write("\nLista original: ");
+        PrintArrayConsole(arrayNumber);
+
+        ChangeElement(arrayNumber);
+
+        arrayNumber = RemoveElement(arrayNumber, 4);
+
+        Console.Write("\nNova lista: ");
+        PrintArrayConsole(arrayNumber);
+
+        Console.Write("\nSoma dos valores da nova lista: " + SumAllElements(arrayNumber));
+
+        Console.ReadLine();
+    }
+
+    private static int[] RemoveElement(int[] array, int valueToRemove)
+    {
+        var count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] != valueToRemove)
+            {
+                array[count++] = array[i];
+            }
+        }
+
+        Array.Resize(ref array, count);
+        
+        return array;
+    }
+
+    private static int[] ChangeElement(int[] array) 
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == 9)
+            {
+                array[i] = 5;
+            }
+        }
+
+        return array;
+    }
+
+    private static int SumAllElements(int[] array) 
+    {
+        var sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    private static void PrintArrayConsole(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
